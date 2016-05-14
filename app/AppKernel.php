f03e3cb3754c29569ca7b305a21f70328e086074
Mikola Peter <mikopet@gmail.com>
2016-05-14 18:14:08 +0200

    default FOS UserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Cocur\Slugify\Bridge\Symfony\CocurSlugifyBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new WebBundle\WebBundle(),
             new GameBundle\GameBundle(),
         ];
