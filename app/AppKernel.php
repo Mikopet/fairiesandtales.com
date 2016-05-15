7892a2e221cae05833f506a514f861b417ebef26
Mikola Peter <mikopet@gmail.com>
2016-05-15 14:28:56 +0200

    Added AdminBundle, modifying UserBundle, modifying WebBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new WebBundle\WebBundle(),
             new GameBundle\GameBundle(),
+            new AdminBundle\AdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
