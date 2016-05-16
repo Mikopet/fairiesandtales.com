0e19aedd136eaaac4c18a643308ff50639c2f573
Mikola Peter <mikopet@gmail.com>
2016-05-16 11:12:39 +0200

    UserBundle && AdminBundle frontend

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -4,14 +4,25 @@ namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
+     * @Template()
      */
     public function indexAction()
     {
-        return $this->render('AdminBundle:Default:index.html.twig');
+        return;
+    }
+
+    /**
+     * @Route("/docs")
+     * @Template()
+     */
+    public function docsAction()
+    {
+        return;
     }
 }
